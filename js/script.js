602092dfd82977ecedbec29331e1af1ec96c79fe
Scarlett Spade <78459369+Scarlett-Spade@users.noreply.github.com>
2021-10-11 21:57:41 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var groceries = [
+  "milk",
+  "coffee",
+  "oranges",
+  "yogourt",
+  "bread",
+  "juice",
+  "lettuce",
+  "flour",
+  "apples",
+  "milk",
+  "cat food",
+  "juice",
+  "ice cream",
+  "potatoes",
+  "bananas",
+  "coffee",
+  "olive oil",
+  "mustard"
+];
+
+console.log(groceries.length);
+
+let deleteDuplicates = function (list) {
+  let cleanList = [];
+
+  for (let item of list) {
+    if (!cleanList.includes(item)) {
+      cleanList.push(item);
+    }
+  }
+
+  return cleanList;
+};
+
+let newGroceries = deleteDuplicates(groceries);
+console.log(newGroceries.length);
+
+newGroceries.forEach(function (item, index) {
+  console.log(`#${index + 1} - ${item}`);
+});
